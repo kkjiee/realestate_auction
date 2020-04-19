92ef4b60fea027f1cd571a06dccd9f4d972c3ed6
spartacoding-7th-kim <spartacoding.7th.kim@gmail.com>
2020-04-19 22:23:01 +0900

    init

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+app1 = Flask("my app")
+from flask import render_template
+
+@app1.route('/')
+def home():
+   return 'This is Home!'
+
+
+@app1.route('/b')
+def b_home():
+   return render_template('b.html')
+
+@app1.route('/b-1')
+def b_1_home():
+   return render_template('b-1.html')
+
+@app1.route('/b-2')
+def b_2_home():
+   return render_template('b-2.html')
+
+@app1.route('/b-3')
+def b_3_home():
+   return render_template('b-3.html')
+
+@app1.route('/b-4')
+def b_4_home():
+   return render_template('b-4.html')
+
+@app1.route('/path1')
+def path1():
+   return 'This is path1!'
+
+
+@app1.route('/in')
+def index():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app1.run("0.0.0.0", port=5000,debug=True)
