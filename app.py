7d6b7cb0a1e700f738da7120e4f095134c72a915
spartacoding-7h-kim <61879561+spartacoding-7h-kim@users.noreply.github.com>
2020-04-25 19:18:00 +0900

    간단하게 계산하기 성공

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app1 = Flask("my app")
-from flask import render_template
+from flask import render_template,  request, jsonify
 
 @app1.route('/')
 def home():
@@ -31,6 +31,42 @@ def b_4_home():
 def path1():
    return 'This is path1!'
 
+
+@app1.route('/calculate_expected_value', methods=['GET'])
+def calculate_expected_value():
+   input1 = request.args.get('input1')
+   input2 = request.args.get('input2')
+   input3 = request.args.get('input3')
+   input4 = request.args.get('input4')
+   input5 = request.args.get('input5')
+   input6 = request.args.get('input6')
+   input7 = request.args.get('input7')
+   input8 = request.args.get('input8')
+   input9 = request.args.get('input9')
+   input10 = request.args.get('input10')
+   input11 = request.args.get('input11')
+   input12 = request.args.get('input12')
+   input13 = request.args.get('input13')
+
+   expected_value = int(input1) - int(input2) - int(input3) - int(input4) - int(input5) - int(input6) - int(input7) - int(input8) - int(input9) - int(input10) - int(input11) - int(input12) - int(input13)
+
+
+   print(input1)
+   print(input2)
+   print(input3)
+   print(input4)
+   print(input5)
+   print(input6)
+   print(input7)
+   print(input8)
+   print(input9)
+   print(input10)
+   print(input11)
+   print(input12)
+   print(input13)
+
+   data = {"expected_value": expected_value}
+   return jsonify(data)
 
 @app1.route('/in')
 def index():
