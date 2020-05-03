ef11044f2cbf63c6c7c651bc6d8ea3529a892dbb
spartacoding-7h-kim <61879561+spartacoding-7h-kim@users.noreply.github.com>
2020-05-03 15:26:47 +0900

    산출세액 + 지방소득세 계산 결과 보이기

--- app.py
+++ app.py
@@ -79,29 +79,12 @@ def calculate_expected_value():
 
 @app1.route('/assignment_value', methods=['GET'])
 def assignment_value():
-   input1 = request.args.get('input1')
-   input2 = request.args.get('input2')
-   input3 = request.args.get('input3')
-   input4 = request.args.get('input4')
-   input5 = request.args.get('input5')
-   input6 = request.args.get('input6')
-   input7 = request.args.get('input7')
-   input8 = request.args.get('input8')
    input9 = request.args.get('input9')
    input10 = request.args.get('input10')
 
 
-  #수정필요 expected_value = int(input1) - int(input2) - int(input3) - int(input4) - int(input5) - int(input6) - int(input7) - int(input8) - int(input9) - int(input10)
+   expected_value = int(input9) + int(input10)
 
-
-   print(input1)
-   print(input2)
-   print(input3)
-   print(input4)
-   print(input5)
-   print(input6)
-   print(input7)
-   print(input8)
    print(input9)
    print(input10)
 
